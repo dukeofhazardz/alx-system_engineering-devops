6928ee53c06a7cb7ed5ee0d24eeb928eafbb5c1a
Daniel John <dj71286@gmail.com>
2023-05-01 17:09:44 +0100

    added puppet manifesto

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,21 @@
+# A Puppet manifesto to automate the task of creating a custom HTTP header response
+
+# Install Nginx
+class { 'nginx': }
+
+# Define custom header
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "add_header X-Served-By ${hostname};",
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  require => Class['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Restart Nginx service to apply changes
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
