849f6c9fe7e63d47c1f6996cd0b813c1ae7d70bd
Daniel John <dj71286@gmail.com>
2023-05-02 02:39:47 +0100

    fix at 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -9,7 +9,7 @@ package { 'nginx':
 file { '/etc/nginx/sites-available/default':
   ensure  => present,
   content => "add_header X-Served-By ${hostname};"
-  match   => '^#\s*$',
+  match   => '^# custom headers$\n',
   mode    => '0644',
   owner   => 'root',
   group   => 'root',
