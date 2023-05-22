a70699f03b16bd1c6106424d3bb4fa4230a837c1
Daniel John <dj71286@gmail.com>
2023-05-22 22:27:30 +0100

    exported to json

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Python script to export data in the CSV format. """
+
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    user = requests.get(url + "/users/{}".format(id)).json()
+    todos = requests.get(url + "/todos", params={'userId': id}).json()
+
+    username = user.get('username')
+
+    e_dict = {
+            "{}".format(id): [{
+                "task": "{}".format(task.get("title")),
+                "completed": "{}".format(task.get("completed")),
+                "username": "{}".format(username)} for task in todos]
+    }
+
+    jsonf = json.dumps(e_dict)
+    with open('{}.json'.format(str(id)), 'w', newline='') as jsonfile:
+        jsonfile.write(jsonf)
+
+    jsonfile.close()
